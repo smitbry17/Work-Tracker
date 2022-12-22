4f9051f8a4b6b8e999f1eeec0bfdc9323d1e290f
Bryan Smith <smitbry17@gmail.com>
2022-12-21 17:20:41 -0800

    finished code for keeping track of and updating hour of day and the cells by past, present or future

--- Develop/script.js
+++ Develop/script.js
@@ -18,17 +18,22 @@ $(document).ready(function () {
 
     var currentHour = dayjs().hour();
 
-    
+    $('.time-block').each(function () {
+      var hour = parseInt($(this).attr('id').split('-')[1]);
+
+      if (hour < currentHour) {
+        $(this).addClass('past');
+      } else if (hour === currentHour){
+        $(this).addClass('present');
+      } else if (hour > currentHour) {
+        $(this).addClass('future');
+      }
+    });
   }
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  hourUpdater();
+
+  setInterval(hourUpdater, 5000);
+
+
 });

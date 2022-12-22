22b6390735f2f3006e8e9a3bd014a672e9067eba
Bryan Smith <smitbry17@gmail.com>
2022-12-21 17:54:20 -0800

    finalized code and everything should be functioning as supposed to

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,35 @@
 $(document).ready(function () {
-
-  $('.saveBtn').on('click', function () {
-
-    var value = $(this).siblings('.description').val();
-    var time = $(this).parent().attr('id');
-
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log("saved");
     localStorage.setItem(time, value);
 
-    $('.notification').addClass('show');
+    $(".notification").addClass("show");
 
     setTimeout(function () {
-      $('.notification').removeClass('show');
+      $(".notification").removeClass("show");
     }, 5000);
   });
 
   function hourUpdater() {
-
     var currentHour = dayjs().hour();
 
-    $('.time-block').each(function () {
-      var hour = parseInt($(this).attr('id').split('-')[1]);
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+      console.log(hourBlock);
 
-      if (hour < currentHour) {
-        $(this).addClass('past');
-      } else if (hour === currentHour){
-        $(this).addClass('present');
-      } else if (hour > currentHour) {
-        $(this).addClass('future');
+      // check if we've moved past this time
+      if (hourBlock < currentHour) {
+        $(this).addClass("past");
+      } else if (hourBlock === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
       }
     });
   }
@@ -35,5 +38,15 @@ $(document).ready(function () {
 
   setInterval(hourUpdater, 5000);
 
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
 
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });

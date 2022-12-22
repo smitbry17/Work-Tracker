7315d75117471b4d642624fd13ac6061fe888470
Bryan Smith <smitbry17@gmail.com>
2022-12-21 17:02:56 -0800

    added code for initial localstorage save function

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,18 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+$(document).ready(function () {
+
+  $('.saveBtn').on('click', function () {
+
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, value);
+
+    $('.notification').addClass('show');
+
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 5000);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
